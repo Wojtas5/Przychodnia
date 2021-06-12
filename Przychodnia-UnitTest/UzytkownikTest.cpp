d5921b7cfd0fe84bafa9b2f9b326cb856fd6a3d4
Judek99 <d.sawicki1999@gmail.com>
2021-06-12 16:23:43 +0200

    Implement UnitTest for uzytkownik class

--- Przychodnia-UnitTest/UzytkownikTest.cpp
+++ Przychodnia-UnitTest/UzytkownikTest.cpp
@@ -15,3 +15,60 @@ TEST(UzytkownikTest, PoprawneLogowanie)
 
 	EXPECT_TRUE(poprawne_zalogowanie);
 }
+
+TEST(UzytkownikTest, NiepoprawnyLogin)
+{
+	string poprawny_login = "DobryLogin";
+	string niepoprawny_login = "ZlyLogin";
+	string poprawne_haslo = "DobreHaslo";
+
+	Uzytkownik uz(poprawny_login, poprawne_haslo, "Marian", "Kowalski", "Prosta", "12345", "997");
+
+	bool niepoprawne_zalogowanie = false;
+
+	niepoprawne_zalogowanie = uz.zaloguj(niepoprawny_login, poprawne_haslo);
+
+	EXPECT_FALSE(niepoprawne_zalogowanie);
+}
+
+TEST(UzytkownikTest, NiepoprawneHaslo)
+{
+	string poprawny_login = "DobryLogin";
+	string poprawne_haslo = "DobreHaslo";
+	string niepoprawne_haslo = "ZleHaslo";
+
+	Uzytkownik uz(poprawny_login, poprawne_haslo, "Marian", "Kowalski", "Prosta", "12345", "997");
+
+	bool niepoprawne_zalogowanie = false;
+
+	niepoprawne_zalogowanie = uz.zaloguj(poprawny_login, niepoprawne_haslo);
+
+	EXPECT_FALSE(niepoprawne_zalogowanie);
+}
+
+TEST(UzytkownikTest, WylogowanieZalogowanego)
+{
+	bool zalogowanie = false;
+
+	string poprawny_login = "DobryLogin";
+	string poprawne_haslo = "DobreHaslo";
+
+	Uzytkownik uz(poprawny_login, poprawne_haslo, "Marian", "Kowalski", "Prosta", "12345", "997");
+
+	uz.zaloguj(poprawny_login, poprawne_haslo);
+	zalogowanie = uz.getZalogowany();
+	EXPECT_TRUE(zalogowanie);
+}
+
+TEST(UzytkownikTest, WylogowanieNiezalogowanego)
+{
+	bool zalogowanie = false;
+
+	string poprawny_login = "DobryLogin";
+	string poprawne_haslo = "DobreHaslo";
+
+	Uzytkownik uz(poprawny_login, poprawne_haslo, "Marian", "Kowalski", "Prosta", "12345", "997");
+
+	zalogowanie = uz.getZalogowany();
+	EXPECT_FALSE(zalogowanie);
+}
